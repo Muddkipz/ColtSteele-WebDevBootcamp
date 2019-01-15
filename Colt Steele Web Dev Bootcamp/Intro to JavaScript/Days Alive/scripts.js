25d902ac14d8f8bdbc743ab0f16b8b1f30ac9e96
Muddkipz <38772060+Muddkipz@users.noreply.github.com>
2019-01-15 18:51:49 -0500

    intro to JS exercises

---
+++ Colt Steele Web Dev Bootcamp/Intro to JavaScript/Days Alive/scripts.js
@@ -0,0 +1,4 @@
+var age = prompt("How old are you?");
+var daysAlive = age * 365;
+
+alert("You have been alive for " + daysAlive + " days");
---
+++ Colt Steele Web Dev Bootcamp/Intro to JavaScript/Stalker Excerceise/script.js
@@ -0,0 +1,5 @@
+var firstName = prompt("Please tell me your first name");
+var lastName = prompt("Please tell me your last name");
+var age = prompt("Please tell me your age");
+console.log("Your first name is " + firstName + " your last name is " + lastName);
+console.log("You are " + age);
